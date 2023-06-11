4888b045225aad842250770f72cbc4dde96c2fe7
董聿凌 <gb2013429@gmail.com>
2023-06-11 17:18:01 +0800

    RWD+pricehtml+jq

---
+++ js/swipper.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    // spaceBetween: 1,
+});
+// const swiper2 = new Swiper('.myswiper2', {
+//     slidesPerView: 1,
+//     // spaceBetween: 1,
+//     // scrollbar: {
+//     //   el: ".swiper-scrollbar",
+//     //   hide: true,
+//     // },
+    
+//   })
+const swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 1,
+    // 分頁   
+    pagination: {
+       el: '.swiper-pagination',
+    },
+  });
+  
+  
