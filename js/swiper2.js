2c3af3a402ca952c7e36283e181b4b0b64bab4b4
董聿凌 <gb2013429@gmail.com>
2023-06-16 00:20:17 +0800

    修改recommed區塊swiper等高+分類獵區塊swiper等寬

---
+++ js/swiper2.js
@@ -0,0 +1,23 @@
+// var swiper3 = new Swiper(".mySwiper3", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   /* 斷點設定 */
+//   breakpoints: {
+//     /* 螢幕寬度大於等於 992px 時切換為 3 欄 */
+//     992: {
+//       slidesPerView: 3,
+//     },
+//     /* 螢幕寬度大於等於 768px 時切換為 2 欄 */
+//     576: {
+//       slidesPerView: 2,
+//     },
+//     /* 更小時都顯示 1 欄 */
+//     0: {
+//       slidesPerView: 1,
+//     },
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
--- js/swipper.js
+++ js/swipper.js
@@ -1,6 +1,20 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
-  // spaceBetween: 1,
+  spaceBetween: 1,
+  breakpoints: {
+    /* 螢幕寬度大於等於 992px 時切換為 3 欄 */
+    992: {
+      slidesPerView: 3,
+    },
+    /* 螢幕寬度大於等於 768px 時切換為 2 欄 */
+    576: {
+      slidesPerView: 2,
+    },
+    /* 更小時都顯示 1 欄 */
+    0: {
+      slidesPerView: 1,
+    },
+  },
 });
 // const swiper2 = new Swiper('.myswiper2', {
 //     slidesPerView: 1,
@@ -13,7 +27,46 @@ var swiper = new Swiper(".mySwiper", {
 //   })
 var swiper2 = new Swiper(".mySwiper2", {
   slidesPerView: 1,
+  /* 斷點設定 */
+  breakpoints: {
+    /* 螢幕寬度大於等於 992px 時切換為 3 欄 */
+    992: {
+      slidesPerView: 3,
+    },
+    /* 螢幕寬度大於等於 768px 時切換為 2 欄 */
+    576: {
+      slidesPerView: 2,
+    },
+    /* 更小時都顯示 1 欄 */
+    0: {
+      slidesPerView: 1,
+    },
+  },
   pagination: {
     el: ".swiper-pagination",
+  },
+});
+
+var swiper3 = new Swiper(".mySwiper3", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  /* 斷點設定 */
+  breakpoints: {
+    /* 螢幕寬度大於等於 992px 時切換為 3 欄 */
+    992: {
+      slidesPerView: 3,
+    },
+    /* 螢幕寬度大於等於 768px 時切換為 2 欄 */
+    576: {
+      slidesPerView: 2,
+    },
+    /* 更小時都顯示 1 欄 */
+    0: {
+      slidesPerView: 1,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
